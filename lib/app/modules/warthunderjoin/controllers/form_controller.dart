45f2010f638f95929441109244f2ba5f4ee28af7
Nicholaus Detrama <nicholausd45@gmail.com>
2022-10-03 09:31:34 +0700

    third commit

--- lib/app/modules/warthunderjoin/controllers/form_controller.dart
+++ lib/app/modules/warthunderjoin/controllers/form_controller.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -10,6 +11,8 @@ class FormController extends GetxController {
   late TextEditingController userController;
   final rulesChecked = false.obs;
   final sbChecked = false.obs;
+  late Uri url;
+  final invite = 'https://discord.gg/8Vq6sBGQVH'.obs;
 
   FirebaseFirestore firestore = FirebaseFirestore.instance;
 
@@ -22,9 +25,9 @@ class FormController extends GetxController {
     CollectionReference formulir = firestore.collection('Formulir');
 
     formulir.add({
-      'Nama Panggilan': nama,
-      'WT nickname': nick,
-      'Negara utama': negara,
+      'Nama': nama,
+      'nickname': nick,
+      'Negara': negara,
       'Discord': user,
     });
   }
@@ -35,14 +38,37 @@ class FormController extends GetxController {
     nickController = TextEditingController();
     negaraController = TextEditingController();
     userController = TextEditingController();
+    url = Uri.parse('$invite');
     super.onInit();
   }
 
-  void isChecked(
+  @override
+  void onClose() {
+    namaController.dispose();
+    nickController.dispose();
+    negaraController.dispose();
+    userController.dispose();
+
+    super.onClose();
+  }
+
+  void formSend(
     bool isRulesChecked,
     bool isSbChecked,
   ) {
+    namaController.clear();
+    negaraController.clear();
+    nickController.clear();
+    userController.clear();
     rulesChecked(isRulesChecked);
     sbChecked(isSbChecked);
+  }
+
+  void discordUrl() async {
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
